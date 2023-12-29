47caee538efcbcccd8001de765541827b9949e18
BCascante <benjamindavid.cascante@gmail.com>
2023-12-28 23:22:03 -0600

    Third Commit

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st 
-st.title("Proyecto")
-st.header("Heloo0")
-st.button("Bye")
+import pandas as pd 
+import plotly_express 
+
+car_data = pd.read_csv('C:/Users/User/OneDrive/Data Analyst/GitHub/quinto_proyecto_da/vehicles_us.csv')
+hist_button = st.button('Construir histograma') # crear un botón
+        
+if hist_button: 
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True) 
+    
